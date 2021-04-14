e572f81ae689353f92ac5c45981473821f94e99e
IrvingPWA <82490186+IrvingPWA@users.noreply.github.com>
2021-04-14 01:10:26 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,10 +1,9 @@
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyDL71Mq-MsW2d8Y8zdEZJjoug_KJc90gwI",
-  authDomain: "aypmbd.firebaseapp.com",
-  databaseURL: "https://aypmbd.firebaseio.com",
-  projectId: "aypmbd",
-  storageBucket: "aypmbd.appspot.com",
-  messagingSenderId: "71455841334",
-  appId: "1:71455841334:web:873522a51aa7a4b4a60c97"
+  apiKey: "AIzaSyBK-bk7N1UgnHWJZuYKLRxq4x6J49yoODw",
+    authDomain: "pwairving.firebaseapp.com",
+    projectId: "pwairving",
+    storageBucket: "pwairving.appspot.com",
+    messagingSenderId: "139000404910",
+    appId: "1:139000404910:web:09930094cd31af7c5c063f"
 });
