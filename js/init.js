ddfab69ae704b8e0de624d9a2afd02aada00a4f1
IrvingPWA <82490186+IrvingPWA@users.noreply.github.com>
2021-04-14 01:22:15 -0500

    Delete js directory

--- js/init.js
+++
@@ -1,9 +0,0 @@
-// @ts-ignore
-firebase.initializeApp({
-  apiKey: "AIzaSyBK-bk7N1UgnHWJZuYKLRxq4x6J49yoODw",
-    authDomain: "pwairving.firebaseapp.com",
-    projectId: "pwairving",
-    storageBucket: "pwairving.appspot.com",
-    messagingSenderId: "139000404910",
-    appId: "1:139000404910:web:09930094cd31af7c5c063f"
-});
